ac80f1628aa0503ffc7086dd46c6ddfdbfe78136
Bassim <bassim_bashir@hotmail.com>
2016-03-19 20:56:17 +0100

    Added new exceptions for deletiong of DataTransformer and VisualizationMethods

---
+++ src/main/java/de/rwthaachen/openlap/visualizer/core/exceptions/DataTransformerDeletionException.java
@@ -0,0 +1,8 @@
+package de.rwthaachen.openlap.visualizer.core.exceptions;
+
+public class DataTransformerDeletionException extends BaseException {
+
+    public DataTransformerDeletionException(String message) {
+        super(message,DataTransformerDeletionException.class.getSimpleName(),"");
+    }
+}
---
+++ src/main/java/de/rwthaachen/openlap/visualizer/core/exceptions/VisualizationMethodDeletionException.java
@@ -0,0 +1,8 @@
+package de.rwthaachen.openlap.visualizer.core.exceptions;
+
+public class VisualizationMethodDeletionException extends BaseException {
+
+    public VisualizationMethodDeletionException(String message) {
+        super(message,VisualizationMethodDeletionException.class.getSimpleName(),"");
+    }
+}
